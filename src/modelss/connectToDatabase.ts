2191dd86a5e3003e958fb95860d39d278c1c6759
Linus BALLS <linus.bolls@code.berlin>
2023-01-25 22:30:19 +0100

    debug: log statement for SECRET_MONGODB_CONNECTION_STRING env var

--- src/modelss/connectToDatabase.ts
+++ src/modelss/connectToDatabase.ts
@@ -3,5 +3,8 @@ import mongoose from 'mongoose';
 import { SECRET_MONGODB_CONNECTION_STRING } from "$env/static/private"
 
 export default async function connectToDatabase() {
+
+	console.info(`connecting to mongodb at "${SECRET_MONGODB_CONNECTION_STRING}"`)
+
 	await mongoose.connect(SECRET_MONGODB_CONNECTION_STRING, { autoIndex: false });
 }
