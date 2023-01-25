add4755a9b33dabef504b46cec5eaa063d80f93d
Linus BALLS <linus.bolls@code.berlin>
2023-01-25 23:03:29 +0100

    fix: docker-compose

--- src/modelss/connectToDatabase.ts
+++ src/modelss/connectToDatabase.ts
@@ -4,7 +4,5 @@ import { SECRET_MONGODB_CONNECTION_STRING } from "$env/static/private"
 
 export default async function connectToDatabase() {
 
-	console.info(`connecting to mongodb at "${SECRET_MONGODB_CONNECTION_STRING}"`)
-
 	await mongoose.connect(SECRET_MONGODB_CONNECTION_STRING, { autoIndex: false });
 }
